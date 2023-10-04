a6177ee3ce640d1d1b137580bf3caf1094e4957b
chamithgithub <dchamith4@gmail.com>
2023-10-04 21:57:47 +0530

    15day

---
+++ 30DayOfCode/src/Day15/Solution.java
@@ -0,0 +1,54 @@
+package Day15;
+
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+class Solution {
+
+    public static Node insert(Node head, int data) {
+        Node newNode = new Node(data);
+
+        // If the linked list is empty, set the new node as the head
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+
+        return head;
+    }
+
+    public static void display(Node head) {
+        Node start = head;
+        while(start != null) {
+            System.out.print(start.data + " ");
+            start = start.next;
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        Node head = null;
+        int N = sc.nextInt();
+
+        while(N-- > 0) {
+            int ele = sc.nextInt();
+            head = insert(head,ele);
+        }
+        display(head);
+        sc.close();
+    }
+}
